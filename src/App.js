63ae68a3e753864649400f36ce175bc1d637db24
SU06FIALHO <sfialho50@gmail.com>
2025-01-27 22:06:15 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,26 +4,34 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        <a
-          href="https://github.com/SU06FIALHO/weather-react-app"
-          target="blank"
-        >
-          {" "}
-          Open-source
-        </a>{" "}
-        code available, made by
-        <a href="https://github.com/SU06FIALHO?tab=repositories" target="blank">
-          {" "}
-          Susana Fialho{" "}
-        </a>
-        and hosted at
-        <a href="https://app.netlify.com/teams/su06fialho/sites" target="blank">
-          {" "}
-          Netlify.{" "}
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <a
+            href="https://github.com/SU06FIALHO/weather-react-app"
+            target="blank"
+          >
+            {" "}
+            Open-source
+          </a>{" "}
+          code available, made by
+          <a
+            href="https://github.com/SU06FIALHO?tab=repositories"
+            target="blank"
+          >
+            {" "}
+            Susana Fialho{" "}
+          </a>
+          and hosted at
+          <a
+            href="https://app.netlify.com/teams/su06fialho/sites"
+            target="blank"
+          >
+            {" "}
+            Netlify.{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
