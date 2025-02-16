86390c1df9b77aba35e0a485cbb1ca0a169f8e1c
SU06FIALHO <sfialho50@gmail.com>
2025-02-16 21:34:29 +0000

    Added API integration

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         
-        <Weather />
+        <Weather defaultCity="Lisbon" />
         <footer>
           <a
             href="https://github.com/SU06FIALHO/weather-react-app"
